a49823e364fe1c9fe77ad932720330d6637cbe78
andreataglia <drew.taglia@gmail.com>
2018-04-04 11:59:57 +0200

    both galleries in index

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ const app = express();
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
 
-// use it until testing
-//process.env.TEST = true;
-
 
 /////////////////////////////////////////////
 ////////////////// APP.USE //////////////////
@@ -55,27 +52,7 @@ app.post('/contactForm', function(req, res) {
     if (req.body.field3_label != null && req.body.field3_label == "guest_2" && req.body.field3_value != "") {
         ospiti += ", " + req.body.field3_value;
     }
-    if (req.body.field4_label != null && req.body.field4_label == "guest_3" && req.body.field4_value != "") {
-        ospiti += ", " + req.body.field4_value;
-    }
-    if (req.body.field5_label != null && req.body.field5_label == "guest_4" && req.body.field5_value != "") {
-        ospiti += ", " + req.body.field5_value;
-    }
-    if (req.body.field6_label != null && req.body.field6_label == "guest_5" && req.body.field6_value != "") {
-        ospiti += ", " + req.body.field6_value;
-    }
-    if (req.body.field7_label != null && req.body.field7_label == "guest_6" && req.body.field7_value != "") {
-        ospiti += ", " + req.body.field7_value;
-    }
-    if (req.body.field8_label != null && req.body.field8_label == "guest_7" && req.body.field8_value != "") {
-        ospiti += ", " + req.body.field8_value;
-    }
-    if (req.body.field9_label != null && req.body.field9_label == "guest_8" && req.body.field9_value != "") {
-        ospiti += ", " + req.body.field9_value;
-    }
-    if (req.body.field10_label != null && req.body.field10_label == "guest_9" && req.body.field10_value != "") {
-        ospiti += ", " + req.body.field10_value;
-    }
+
     if (req.body.msg != null && req.body.msg != "") {
         messaggio = req.body.msg;
     }
@@ -83,9 +60,9 @@ app.post('/contactForm', function(req, res) {
     // setup e-mail data with unicode symbols
     var mailOptions = {
         from: req.body.fromname + " <annaemasu@info.it>", // sender address
-        to: "anna@gmail.com", // list of receivers
-        subject: "Aò! Hanno scritto da annaemasu.it!", // Subject line
-        html: '<p>Ha scritto <b>' + req.body.fromname + '</b>!! (con mail: ' + req.body.fromemail + ')</p> <p>Alla domanda chi "Ci sei?" ha detto: <b>' + req.body.field0_value + '</b> </p> <p>Parla a nome di: ' + ospiti + '<p> <p>Ha lasciato questo messaggio: ' + messaggio + '<p>'
+        to: "drew.taglia@gmail.com", // list of receivers
+        subject: "Massaggio da Palazzo Alfeo", // Subject line
+        html: '<p>Ha scritto <b>' + req.body.name + '</b>!! (con mail: ' + req.body.fromemail + ')</p> <p>Alla domanda chi "Ci sei?" ha detto: <b>' + req.body.field0_value + '</b> </p> <p>Parla a nome di: ' + ospiti + '<p> <p>Ha lasciato questo messaggio: ' + messaggio + '<p>'
     };
 
     // send mail with defined transport object
